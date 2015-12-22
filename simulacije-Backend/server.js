11d6a10ce89d758a3c280c0e1076e475ddba2a8a
mrki <mladjenovic031@gmail.com>
2015-12-22 16:51:31 +0100

    generate excel added

--- simulacije-Backend/server.js
+++ simulacije-Backend/server.js
@@ -42,13 +42,13 @@ if (isDeveloping) {
 
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
+  app.get('/', cors, function response(req, res) {
     res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
     res.end();
   });
 } else {
   app.use(express.static(__dirname + '/dist'));
-  app.get('*', function response(req, res) {
+  app.get('/', cors, function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
   });
 }
@@ -152,7 +152,40 @@ app.post('/register', cors, function(req, res) {
   });
 });
 
-//LOGING OUT
+//GENERATING REPORT
+app.get('/getReport', cors, function (req, res) {
+  fs.readFile(path.join(__dirname, 'templates', 'template1.xlsx'), function(err, data) {
+    var template = new Xlsx(data);
+    var values = {
+      users: []
+    };
+
+    User.find({}, {name: 1, email: 1, registrationDate: 1, score: 1, role: 1}).lean().exec((err, users) => {
+      if (err) {
+        res.status(404).send('can not get report!');
+    		console.log('DB ERROR');
+    	} else {
+        users.map(user => {
+       let user1 = {};
+        user1.name = user.name;
+        user1.email = user.email;
+        user1.score = user.score;
+        user1.registrationDate = user.registrationDate;
+        values.users.push(user1);
+      });
+     }
+    });
+
+    const sheetNumber = 1;
+
+    template.substitute(sheetNumber, values);
+
+    var data1 = template.generate();
+    res.set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.status(200).send(new Buffer(data1, 'binary'));
+    console.log('REPORT SUCCESSFULLY SENT!');
+});
+});
 
 app.listen(port, '0.0.0.0', function onStart(err) {
   if (err) {
